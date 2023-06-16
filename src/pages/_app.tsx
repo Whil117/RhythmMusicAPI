21110094d4039c0901a1bc2c3858c2ae9ff445e2
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-06-16 17:52:28 -0600

    update with seo page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import AtomSeo from '@Components/index';
 import '@Styles/fonts.css';
 import '@Styles/globals.css';
 import '@Styles/normalize.css';
@@ -18,6 +19,13 @@ const MyApp = ({
 }: AppPropsWithLayout) => {
   return (
     <ApolloProvider client={client}>
+      <AtomSeo
+        url="https://rhythmusic.whil.online/"
+        title="Rhythm Music API"
+        content="Node.js, Graphql, Backend, API"
+        image="/RhtmBg.png"
+        description="The music API also provides access to an extensive music database that includes detailed information about songs, albums, artists, genres, and other related metadata. This allows developers to obtain accurate and up-to-date information about the available music, which helps them create content-rich and relevant applications for music lovers."
+      />
       <AtomWrapper
         padding="4rem"
         justifyContent="center"
